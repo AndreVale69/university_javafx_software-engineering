756dedac011dc79b634f539687522c84d4e34da7
AndreVale69 <57899285+AndreVale69@users.noreply.github.com>
2022-09-03 16:19:10 +0200

    Delete Anagrafica directory

--- Anagrafica/src/main/java/com/univr/anagrafica/Date_enum.java
+++
@@ -1,4 +0,0 @@
-package com.univr.anagrafica;
-
-public enum Date_enum {
-}
--- Anagrafica/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.univr.anagrafica {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.univr.anagrafica to javafx.fxml;
-    exports com.univr.anagrafica;
-    exports com.univr.graphics;
-    opens com.univr.graphics.components.custom to javafx.base;
-}
